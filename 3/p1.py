b71fc4cc285028ca14e3bfcb80ade8c306410b2b
sigma-theta <makifnaymann@gmail.com>
2024-12-03 13:27:10 +0100

    why is part2 not working

---
+++ 3/p1.py
@@ -0,0 +1,12 @@
+import re
+
+lookup_test = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+pattern = "mul\(\s*\d+\s*,\s*\d+\s*\)"
+sum = 0
+
+with open('input.txt', 'r') as input:
+    for l in input.readlines():
+        valid = re.findall(pattern, l)
+        for i in valid:
+            sum += int(i[i.find("(")+1:i.find(",")]) * int(i[i.find(",")+1:i.find(")")])
+print(sum)
---
+++ 3/p2.py
@@ -0,0 +1,4 @@
+a = [1,2]
+b = [3,4]
+a.append(b)
+print(a)
