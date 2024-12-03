127663833a95cd2bb9ef36ad4eba7077153e7cd3
sigma-theta <makifnaymann@gmail.com>
2024-12-03 13:27:28 +0100

    added test input

--- 3/p1.py
+++ 3/p1.py
@@ -1,10 +1,13 @@
 import re
+import os
+
+file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
 
 lookup_test = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 pattern = "mul\(\s*\d+\s*,\s*\d+\s*\)"
 sum = 0
 
-with open('input.txt', 'r') as input:
+with open(file_path, 'r') as input:
     for l in input.readlines():
         valid = re.findall(pattern, l)
         for i in valid:
